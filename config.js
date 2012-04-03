f3eca92d9dc3a0a3e424a9c33dc89a7ec3a3164b
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-03 15:45:53 -0400

    heroku friendly config.js

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ module.exports= {
   "secrets" : {
     "clientId" : process.env.FOURSQUARE_CLIENT_ID,
     "clientSecret" : process.env.FOURSQUARE_CLIENT_SECRET,
-    #"redirectUrl" : "http://hollow-mist-4760.herokuapp.com/callback"
-    "redirectUrl" : "http://falling-ice-9760.herokuapp.com/callback"
+    "redirectUrl" : "http://hollow-mist-4760.herokuapp.com/callback"
   }
 }
