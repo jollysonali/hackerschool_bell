e0dd3aa455d1e36128be0c3f8e47d3bd9d4aba8b
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-03 15:32:31 -0400

    heroku url for callback

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports= {
   "secrets" : {
     "clientId" : process.env.FOURSQUARE_CLIENT_ID,
     "clientSecret" : process.env.FOURSQUARE_CLIENT_SECRET,
-    "redirectUrl" : "http://localhost:3000/callback"
-    //"redirectUrl" : "http://falling-ice-9760.herokuapp.com/callback"
+    #"redirectUrl" : "http://hollow-mist-4760.herokuapp.com/callback"
+    "redirectUrl" : "http://falling-ice-9760.herokuapp.com/callback"
   }
 }
