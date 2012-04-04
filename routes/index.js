a713784d39d071aec24d3a681fd4818b42117bff
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-03 20:00:35 -0400

    added log data for checkins

--- routes/index.js
+++ routes/index.js
@@ -60,7 +60,7 @@ exports.sonalisBadges = function (req, res) {
 exports.hsCheckins = function (req, res, io) {
   console.log(req.body.checkin);
   console.log('this is a console log');
-  io.sockets.emit('checkin');
+  io.sockets.emit('checkin', req.body.checkin);
 };
 
 /*
