16c3603a55191fea17255d366fdb4b4d0678bb94
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-07 17:25:31 -0400

    jade template created

--- routes/index.js
+++ routes/index.js
@@ -22,32 +22,6 @@ exports.index = function(req, res) {
 			res.render('checkins.jade', { title: 'Checkins', checkins: data.hereNow.items });    
     }
   });	
-  /*
-  Foursquare.Users.getBadges(null, aToken, function (error, data) {
-    if(error) {
-      //reportError(test, error.message);
-    }
-    else {
-			var output = [];
-			var imgsrc ='';
-			var b;
-			var img = '';
-			for (badge in data.badges){
-			  //console.log (badge + 'is the badge');
-				b = '';
-				img = '';
-				b = data.badges[badge];
-				img += b.image.prefix + b.image.sizes[0] + b.image.name;
-				
-				//take all the badges and add the image url onto the output array
-				output.push(img);
-			}
-			res.render('badges.jade', { title: 'Badges', badges: output });
-    }
-  });  
-	*/
-	
-	res.render('index.jade', { title: 'Hacker School Checkins' });
   
 };
 
