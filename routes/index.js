0cbaf0e019db2536f6faffc972c7abf19dc9e604
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-07 17:41:56 -0400

    jade template created

--- routes/index.js
+++ routes/index.js
@@ -77,11 +77,6 @@ exports.hsCheckins = function (req, res, io) {
   console.log(req.body.checkin);
   console.log('this is a console log');
   io.sockets.emit('checkin', req.body.checkin);
-
-  //Who is at the venue now
-
-//    https://api.foursquare.com/v2/venues/4f3933eae4b017ad7cdce1fd/herenow?oauth_token=0JQVD5NQSOUWSP1ZBNXFZCLFP11PEA2K0ME03GFP0TLJT54K&v=20120407
-
 };
 
 
@@ -89,55 +84,3 @@ exports.hsCheckins = function (req, res, io) {
 
 - when the page loads get everyone who has checked into the venue 
 - listen and update when new checkins happen.
-var checkin {
-              "id": "4e6fe1404b90c00032eeac34",
-              "createdAt": 1315955008,
-              "type": "checkin",
-              "timeZone": "America/New_York",
-              "user": {
-                  "id": "1",
-                  "firstName": "Jimmy",
-                  "lastName": "Foursquare",
-                  "photo": "https://foursquare.com/img/blank_boy.png",
-                  "gender": "male",
-                  "homeCity": "New York, NY",
-                  "relationship": "self"
-              },
-              "venue": {
-                  "id": "4ab7e57cf964a5205f7b20e3",
-                  "name": "foursquare HQ",
-                  "contact": {
-                      "twitter": "foursquare"
-                  },
-                  "location": {
-                      "address": "East Village",
-                      "lat": 40.72809214560253,
-                      "lng": -73.99112284183502,
-                      "city": "New York",
-                      "state": "NY",
-                      "postalCode": "10003",
-                      "country": "USA"
-                  },
-                  "categories": [
-                      {
-                          "id": "4bf58dd8d48988d125941735",
-                          "name": "Tech Startup",
-                          "pluralName": "Tech Startups",
-                          "shortName": "Tech Startup",
-                          "icon": "https://foursquare.com/img/categories/building/default.png",
-                          "parents": [
-                              "Professional & Other Places",
-                              "Offices"
-                          ],
-                          "primary": true
-                      }
-                  ],
-                  "verified": true,
-                  "stats": {
-                      "checkinsCount": 7313,
-                      "usersCount": 565,
-                      "tipCount": 128
-                  },
-                  "url": "http://foursquare.com"
-              }
-          } */
