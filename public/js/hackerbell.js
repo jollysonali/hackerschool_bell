5312b749d8a53feb148c898f3e4437312f01ef24
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-16 18:22:43 -0400

    stuling my page

--- public/js/hackerbell.js
+++ public/js/hackerbell.js
@@ -1,7 +1,6 @@
 var socket = io.connect('http://'+document.location.host);
 socket.on('checkin', function(data){
   console.log('got a checkin!');
-  //console.log(JSON.parse(data));
   data = JSON.parse(data);
   console.log(data);
   $('#feeds').prepend('<div class="checkin" style="display:none"><img src="' + data.user.photo + '"><p>' + data.user.firstName + '</p></div>');
