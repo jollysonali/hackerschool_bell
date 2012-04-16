ed31427f024a3c698dae63cc2626935c03e641ec
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-16 13:03:36 -0400

    trying to add socket io into my hackerbell.js

--- public/js/hackerbell.js
+++ public/js/hackerbell.js
@@ -2,6 +2,8 @@ var socket = io.connect('http://'+document.location.host);
 socket.on('checkin', function(data){
   console.log('got a checkin!');
   //console.log(JSON.parse(data));
+  data = JSON.parse(data);
+  console.log(data);
   $("#feeds").prepend("<div class='item'><img src='" + data.user.photo + "'><p>" + data.user.firstName + "</p></div>");
 });
 
