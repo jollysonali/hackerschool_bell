0b921e99d51a17a05874679de152d2091d5b634e
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-04-16 17:35:55 -0400

    trying to add socket io into my hackerbell.js

--- public/js/hackerbell.js
+++ public/js/hackerbell.js
@@ -4,28 +4,6 @@ socket.on('checkin', function(data){
   //console.log(JSON.parse(data));
   data = JSON.parse(data);
   console.log(data);
-  $('#feeds').prepend('<div class="item"><img src="' + data.user.photo + '"><p>' + data.user.firstName + '</p></div>');
-});
-
-var delay = 2000; // you can change it
-var count = 5; // How much items to animate
-var showing = 3; //How much items to show at a time
-var i = 0;
-
-function move(i) {
-  return function() {
-    $('#feed'+i).remove().css('display', 'none').prependTo('#feeds');
-    }
-}
-
-function shift() {
-  var toShow = (i + showing) % count;
-  $('#feed'+toShow).slideDown(1000, move(i));
-  $('#feed'+i).slideUp(1000, move(i));
-    i = (i + 1) % count;
-    setTimeout('shift()', delay);
-}    
-
-$(document).ready(function() {
-  setTimeout('shift()', delay);
+  $('#feeds').prepend('<div class="item" style="display:none"><img src="' + data.user.photo + '"><p>' + data.user.firstName + '</p></div>');
+  $('#feeds .item:first-child').slideDown();
 });
