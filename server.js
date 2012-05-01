1ff2bea443b6d97b91ae6f2760affcfada8fa7af
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-05-01 16:08:24 -0400

    io is global

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
-var io = require('socket.io').listen(app);
+io = require('socket.io').listen(app);
 
 // Configuration
 
