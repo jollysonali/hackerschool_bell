c32f58dc0854c8ae462a4b98a4d0d9bcea3078a8
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-05-01 15:14:42 -0400

    added HS logo

--- public/js/hackerbell.js
+++ public/js/hackerbell.js
@@ -5,4 +5,5 @@ socket.on('checkin', function(data){
   console.log(data);
   $('#feeds').prepend('<div class="checkin" style="display:none"><img src="' + data.user.photo + '"><p>' + data.user.firstName + '</p></div>');
   $('#feeds .checkin:first-child').fadeIn(3000);
+  //check feeds if current 
 });
